ca4e8eea473c0985adb28eacc1680355f2af05eb
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-11-17 11:07:45 +0200

    Add files via upload

---
+++ Problema6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Setul1
+{
+    class Problema1
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = int.Parse(Console.ReadLine());
+            b= int.Parse(Console.ReadLine());
+            c= int.Parse(Console.ReadLine());
+            if (a > 0 && b > 0 && c >0)
+            {
+                if (a == b || a == c)
+                {
+                    if (b > a && b > c || c > a && c > b)
+                    {
+                        Console.WriteLine("a b c sunt laturile unui triunghi");
+                    }
+                }
+            }
+        }
+    }
+}
