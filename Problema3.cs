c4d9371b73812bf58bcb3e7fa6083c1924f6e2b2
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-11-17 10:55:17 +0200

    Add files via upload

---
+++ Problema3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Setul1
+{
+    class Problema1
+    {
+        static void Main(string[] args)
+        {
+            int n, k;
+            n = int.Parse(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
+            if(n%k==0)
+            {
+                Console.WriteLine("se divide");
+            }
+            else
+            {
+                Console.WriteLine("nu se divide");
+            }
+        }
+    }
+}
