fa8175b22074767fe1b3ab6ff6ccefefe87cffb9
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-11-17 10:32:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Setul1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x;
+            a = int.Parse(Console.ReadLine());
+            b= int.Parse(Console.ReadLine());
+            x = -b / a;
+            Console.WriteLine(x);
+        }
+    }
+}
