d622c3d4b652f3d164676612fa40a4ea6d35ae7a
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-11-17 10:49:03 +0200

    Add files via upload

---
+++ Problema2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Setul1
+{
+    class Problema1
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, x, delta;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            delta = b * b - 4 * a * c;
+            x = (-b + Math.Sqrt(delta)) / 2*a;
+            Console.WriteLine(x);
+            x = (-b - Math.Sqrt(delta)) / 2 * a;
+            Console.WriteLine(x);
+        }
+    }
+}
