8f39dac78e655b928098c2d1e61443a18a9d983d
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-11-18 11:58:41 +0200

    Add files via upload

---
+++ Problema18.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Setul1
+{
+    class Problema1
+    {
+        static void Main(string[] args)
+        {
+            int n, p = 0, d = 2;
+            n = int.Parse(Console.ReadLine());
+            while(n!=1)
+            {
+                p = 0;
+                while(n%d==0)
+                {
+                    n = n / d;
+                    p++;
+                }
+                if(p!=0)
+                {
+                    Console.Write($"{d}^{p}*");
+                }
+                d++;
+            }
+        }
+    }
+}
