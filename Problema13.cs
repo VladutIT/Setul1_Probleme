01f48b4dcaed8c0d5bb4abc6fc952fbb0a1bc1c4
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-11-18 11:43:09 +0200

    Add files via upload

---
+++ Problema13.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Setul1
+{
+    class Problema1
+    {
+        static void Main(string[] args)
+        {
+            int y1, y2;
+            y1 = int.Parse(Console.ReadLine());
+            y2= int.Parse(Console.ReadLine());
+            for(int i=y2;i<=y2;i++)
+            {
+                if((i%4==0 && i%100!=0)||i%400==0)
+                {
+                    Console.WriteLine($"{i} este an bisect");
+                }
+            }
+        }
+    }
+}
