8ce827cde4270d3e2c30158884ace7648e06c8eb
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-11-18 11:01:37 +0200

    Add files via upload

---
+++ Problema7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Setul1
+{
+    class Problema1
+    {
+        static void Main(string[] args)
+        {
+            int a, b, aux = 0;
+            a = int.Parse(Console.ReadLine());
+            b= int.Parse(Console.ReadLine());
+            aux = a;
+            a = b;
+            b = aux;
+        }
+    }
+}
