f14514e56b90f3b7deb3620b4c9097b262220b75
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-11-18 11:54:34 +0200

    Add files via upload

---
+++ Problema14.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Setul1
+{
+    class Problema1
+    {
+        static void Main(string[] args)
+        {
+            int n, ogl = 0,a=0;
+            n = int.Parse(Console.ReadLine());
+            a = n;
+            while(a!=0)
+            {
+                ogl = ogl * 10 + a % 10;
+                a = a / 10;
+            }
+            if(n==ogl)
+            {
+                Console.WriteLine("numarul este palindrom");
+            }
+        }
+    }
+}
