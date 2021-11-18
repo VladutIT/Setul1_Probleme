b34d18bf2f38478eb88324b411cc3dc68d3a6c99
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-11-18 11:49:37 +0200

    Add files via upload

---
+++ Problema15.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Setul1
+{
+    class Problema1
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = int.Parse(Console.ReadLine());
+            b= int.Parse(Console.ReadLine());
+            c= int.Parse(Console.ReadLine());
+            if(a>=b && a>=c)
+            {
+                Console.WriteLine(a);
+            }
+            if(b>=a && c>=a)
+            {
+                Console.WriteLine(b);
+            }
+            if(c>=b && c>=a)
+            {
+                Console.WriteLine(c); 
+            }
+        }
+    }
+}
