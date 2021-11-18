c555526eba26b5cffea1b810eed9a1965794ae2e
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-11-18 11:34:14 +0200

    Add files via upload

---
+++ Problema11.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Setul1
+{
+    class Problema1
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+            while(n!=0)
+            {
+                Console.Write(n % 10);
+                n = n / 10;
+            }
+            
+        }
+    }
+}
