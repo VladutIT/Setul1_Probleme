f8444551c328274f3dd1e6713a09deefcfd1091f
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-11-18 11:36:29 +0200

    Add files via upload

---
+++ Problema10.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Setul1
+{
+    class Problema1
+    {
+        static void Main(string[] args)
+        {
+            int n,div=0;
+            n = int.Parse(Console.ReadLine());
+           for(int i=1;i<=n;i++)
+            {
+                if(n%i==0)
+                {
+                    div++;
+                }
+            }
+            if(div==2)
+            {
+                Console.WriteLine("numarul este prim");
+            }
+            else
+            {
+                Console.WriteLine("numarul nu este prim");
+            }
+        }
+    }
+}
