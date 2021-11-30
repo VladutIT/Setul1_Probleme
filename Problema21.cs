c2aaa432f3118962e40fac7550531221b7ebc977
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-11-30 14:48:06 +0200

    Add files via upload

---
+++ Problema21.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2611
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int x,a=0;
+            x = int.Parse(Console.ReadLine());
+          for(int i=1;i<=1024;i++)
+            {
+                if(i>x)
+                {
+                    if(i==x)
+                    {
+                        i = x;
+                    }
+                    i = x;
+                    a = i;
+                }
+            }
+            Console.WriteLine($"Numarul este {a}");
+        }
+
+    }
+}
