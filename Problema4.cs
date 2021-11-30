d0306f3ce681642ea1ab9c545491baf53532d24d
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-11-30 14:44:23 +0200

    Add files via upload

---
+++ Problema4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2611
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+            if((n % 4 == 0 && n % 100 != 0) || n % 400 == 0)
+            {
+                Console.WriteLine("Anul este bisect");
+            }
+            else
+            {
+                Console.WriteLine("Anul nu este bisect");
+            }
+        }
+
+    }
+}
